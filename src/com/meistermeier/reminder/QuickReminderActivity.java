3136f1691e81124285652225929c8f7e8b178289
Gerrit Meier <meistermeier@gmail.com>
2011-01-09 18:48:58 +0100

    Added the list view with a simple adapter

--- src/com/meistermeier/reminder/QuickReminderActivity.java
+++ src/com/meistermeier/reminder/QuickReminderActivity.java
@@ -1,15 +1,26 @@
 package com.meistermeier.reminder;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class QuickReminderActivity extends Activity
 {
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        setupMainListView();
+    }
+
+    private void setupMainListView() {
+        ListView mainListView = (ListView) findViewById(R.id.mainListView);
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1 ,new String[]{"Test","Wurst"});
+        mainListView.setAdapter(arrayAdapter);
+
     }
 }
