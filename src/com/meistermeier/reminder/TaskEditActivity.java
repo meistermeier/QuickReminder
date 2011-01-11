2080f6d9206c61a764e71d8b9d38586260517862
Gerrit Meier <meistermeier@gmail.com>
2011-01-11 16:00:51 +0100

    Added additional fields to TaskEditActivity and transfer of existing data to "edit mode"

--- src/com/meistermeier/reminder/TaskEditActivity.java
+++ src/com/meistermeier/reminder/TaskEditActivity.java
@@ -2,7 +2,13 @@ package com.meistermeier.reminder;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Some nice information
@@ -13,19 +19,45 @@ public class TaskEditActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.task_edit);
 
-        Bundle extras = getIntent().getExtras();
-
-        if (extras != null) {
-            fillWithExtraData();
+        // if a extra set -> fill the components with data
+        if (getIntent().hasExtra(TaskItem.NAME_FIELD)) {
+            fillComponentsWithData();
         }
 
     }
 
-    private void fillWithExtraData() {
+    private void fillComponentsWithData() {
         Bundle extras = getIntent().getExtras();
 
+        // prepare components from edit view
         EditText nameEditText = (EditText) findViewById(R.id.task_edit_name);
-        nameEditText.setText((String)extras.get(TaskItem.NAME_FIELD));
+        CheckBox checkBox = (CheckBox) findViewById(R.id.task_edit_reminder);
+        DatePicker datePicker = (DatePicker) findViewById(R.id.task_edit_date);
+        TimePicker timePicker = (TimePicker) findViewById(R.id.task_edit_time);
+
+        // prepare data
+        Date dueDate = (Date) extras.get(TaskItem.DUE_DATE_FIELD);
+        Calendar dueCalendar = Calendar.getInstance();
+        dueCalendar.setTimeInMillis(dueDate.getTime());
+
+        String taskName = (String) extras.get(TaskItem.NAME_FIELD);
+        Boolean reminder = (Boolean) extras.get(TaskItem.REMINDER_FIELD);
+
+        // data for date picker
+        int day = dueCalendar.get(Calendar.DAY_OF_MONTH);
+        int month = dueCalendar.get(Calendar.MONTH);
+        int year = dueCalendar.get(Calendar.YEAR);
+
+        // data for time picker
+        int hour = dueCalendar.get(Calendar.HOUR);
+        int minute = dueCalendar.get(Calendar.MINUTE);
+
+        // populate the data to the components
+        nameEditText.setText(taskName);
+        checkBox.setChecked(reminder);
+        datePicker.updateDate(year, month, day);
+        timePicker.setCurrentHour(hour);
+        timePicker.setCurrentMinute(minute);
     }
 
 }
