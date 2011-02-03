1c899ff043571ff357baf88023e67407e0fe53d5
Gerrit Meier <meistermeier@gmail.com>
2011-02-03 17:47:23 +0100

    Setting data / uri on the intents to make them unique.

--- src/com/meistermeier/reminder/TaskNotificationReceiver.java
+++ src/com/meistermeier/reminder/TaskNotificationReceiver.java
@@ -6,7 +6,8 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
+import android.net.Uri;
+import android.util.Log;
 
 /**
  */
@@ -20,18 +21,23 @@ public class TaskNotificationReceiver extends BroadcastReceiver {
         Notification notification = new Notification(icon, "things to do", System.currentTimeMillis());
 
         CharSequence contentTitle = "QuickReminder";
+
         CharSequence contentText = intent.getExtras().getString("taskname");
         long taskNotificationId = intent.getExtras().getLong("taskid");
 
+        Log.d("QuickReminder", "Creating notification with id " + (int) taskNotificationId);
+
         Intent notificationIntent = new Intent(context, QuickReminderActivity.class);
         notificationIntent.putExtra("tasknotificationid", taskNotificationId);
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        notificationIntent.setData(Uri.parse("quickReminder://" + taskNotificationId));
 
         PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
 
         notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
 
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify((int)taskNotificationId, notification);
+        notificationManager.notify((int) taskNotificationId, notification);
 
     }
 
